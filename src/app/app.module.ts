6b9f37af5ac8e5f7ec4943d419ce7a52f5828fed
Dmitry Voronov <voronov.dmitry@gmail.com>
2020-06-16 13:45:24 -0400

    navbar is ready all components are working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemsComponent } from './items/items.component';
+import { ItemsListComponent } from './items/items-list/items-list.component';
+import { ItemsItemComponent } from './items/items-list/items-item/items-item.component';
+import { ReviewComponent } from './review/review.component';
+import { ReviewEditComponent } from './review/review-edit/review-edit.component';
+import { HeadingComponent } from './heading/heading.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsComponent,
+    ItemsListComponent,
+    ItemsItemComponent,
+    ReviewComponent,
+    ReviewEditComponent,
+    HeadingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/items/items-list/items-item/items-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-items-item',
+  templateUrl: './items-item.component.html',
+  styleUrls: ['./items-item.component.css']
+})
+export class ItemsItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
